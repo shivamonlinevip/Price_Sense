9cf12e84c4ffbec4e682933d358d5a1991b66b58
shivamonlinevip <shivamonline.vip@gmail.com>
2025-10-29 21:02:16 +0530

    Delete PriceSense directory

--- PriceSense/backend/models/productModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const PriceSchema = new mongoose.Schema({
-  product: { type: String, index: true },
-  price: Number,
-  source: String,
-  date: { type: Date, default: Date.now }
-});
-export default mongoose.model('PriceRecord', PriceSchema);
